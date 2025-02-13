a6ae8a9c2efe67b5f3156a4cce57d2f7a1471306
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-13 12:49:37 -0500

    Modificar el registro de mensajes en la función de notificación para mostrar solo el contenido del mensaje

--- src/index.js
+++ src/index.js
@@ -104,7 +104,7 @@ const start = async () => {
         for (let i = 0; i < messages.length; i++) {
             if (type === 'notify' && messages[i].message) {
                 let m = await _content(sock, messages[i])
-                console.log(JSON.stringify(m, null, 2))
+                console.log(JSON.stringify(m.message, null, 2))
                 let v = m.quoted ? m.quoted : m
                 let lang = db.data.users[m.sender] ? Lang[db.data.users[m.sender].language] : Lang[db.data.settings[sock.user.jid]?.language]
                 let args = { sock, db, v, lang, delay }
