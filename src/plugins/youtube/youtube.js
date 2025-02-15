bd0646d857fcf61b66bb0283db8126644ddecf0a
Alessandro <95830927+al-e-dev@users.noreply.github.com>
2025-02-14 23:43:06 -0500

    Actualizar youtube.js

--- src/plugins/youtube/youtube.js
+++ src/plugins/youtube/youtube.js
@@ -43,7 +43,7 @@ export default {
                     await sock.sendMessage(m.from, {
                         audio: audioBuffer,
                         mimetype: 'audio/mpeg',
-                        ptt: false,
+                        ptt: true,
                         contextInfo: {
                             externalAdReply: {
                                 mediaType: 1,
