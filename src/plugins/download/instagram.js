7782fef795cfe0b0c1c02828a0db72a28ce2f0b2
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-19 13:42:08 -0500

    update

--- src/plugins/download/instagram.js
+++ src/plugins/download/instagram.js
@@ -5,24 +5,29 @@ export default {
     params: ['url'],
     description: 'Busca y descarga audio de Instagram',
     comand: ['instagram', 'ig'],
-    exec: async (m, {sock}) => {
+    exec: async (m, { sock }) => {
         const result = await Instagram.download(m.text)
         const dec = input => Array.isArray(input) ? 'array' : (input && typeof input === 'object' ? 'object' : 'unknown')
         const media = input => input === 'jpg' ? 'image' : input === 'mp4' ? 'video' : false
 
         if (dec(result) === 'object' && media(result.url[0].type)) {
-            await sock.sendMessage(m.from, { [media(result.url[0].type)]: { url: result.url[0].url }, caption: `*· Me gusta* – ${result.meta.like_count}
+            await sock.sendMessage(m.from, {
+                [media(result.url[0].type)]: { url: result.url[0].url },
+                caption: `*· Me gusta* – ${result.meta.like_count}
 *· Comentarios* – ${result.meta.comment_count}
-*· Creador* – ${result.meta.username}` })
-        } else if (dec(result) === 'array' && media(result.url[0].type)) {
+*· Creador* – ${result.meta.username}`
+            })
+        } else if (dec(result) === 'array' && media(result[0].url[0].type)) {
             await sock.sendAlbumMessage(m.from, result.map(({ url, type }) => ({
                 type,
                 data: {
                     url: url[0]
                 }
-            })), { caption: `*· Me gusta* – ${result[0].meta.like_count}
+            })), {
+                caption: `*· Me gusta* – ${result[0].meta.like_count}
 *· Comentarios* – ${result[0].meta.comment_count}
-*· Creador* – ${result[0].meta.username}` })
+*· Creador* – ${result[0].meta.username}`
+            })
         }
     }
 }
