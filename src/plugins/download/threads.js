ce5e3c56eaff87020aaff307c0f77d5a91fa3c85
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-20 22:32:21 -0500

    update

--- src/plugins/download/threads.js
+++ src/plugins/download/threads.js
@@ -8,6 +8,8 @@ export default {
     exec: async (m, { sock }) => {
         const thread = await Threads.download(m.text)
 
+        console.log(JSON.stringify(thread, null, 2))
+
         if (Array.isArray(thread.download)) {
             await sock.sendAlbumMessage(
                 m.from,
