ee7d16a386b2b011486b44497180e5a0f3b9fb3c
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-26 19:26:42 -0500

    update

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const start = async () => {
     store.bind(sock.ev);
     sock.ev.on("creds.update", saveCreds)
     if (!sock.authState.creds.registered) {
-        console.log(`Emparejamiento con este código: ${await sock.requestPairingCode(await question("Ingresa tu número de WhatsApp activo: "))}`)
+        console.log(`Emparejamiento con este código: ${await sock.requestPairingCode(await question("Ingresa tu número de WhatsApp activo: "), "KAORINET")}`)
     }
     
     sock.ev.on("connection.update", ({ connection, lastDisconnect }) => {
