36db1f9a52b9c2dc5154eb6ffd64efa00845b163
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-27 16:02:15 -0500

    update

--- src/scraper/youtube.js
+++ src/scraper/youtube.js
@@ -42,8 +42,7 @@ export default new class Download {
                     "sec-fetch-dest": "empty",
                     "sec-fetch-mode": "cors",
                     "sec-fetch-site": "same-origin",
-                    "user-agent":
-                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
                 },
                 params: { search_query: query }
             }).then(({ data }) => {
@@ -89,8 +88,7 @@ export default new class Download {
                     "sec-fetch-dest": "empty",
                     "sec-fetch-mode": "cors",
                     "sec-fetch-site": "same-origin",
-                    "user-agent":
-                        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
                 },
                 params: { v: id }
             }).then(async ({ data }) => {
