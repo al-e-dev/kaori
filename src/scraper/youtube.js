72abe24d6b3ffde2c94d8e18b5f495d4ca4151b6
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-27 16:14:32 -0500

    update

--- src/scraper/youtube.js
+++ src/scraper/youtube.js
@@ -76,21 +76,10 @@ export default new class Download {
     }
 
     getInfo(url) {
-        return new Promise(async resolve => {
+        return new Promise(async (resolve, reject) => {
             const id = this.getYouTubeID(url)
-            await this.client.get(`https://www.youtube.com/watch`, {
-                headers: {
-                    accept: "*/*",
-                    "accept-encoding": "gzip, deflate, br",
-                    "accept-language": "en-US,en;q=0.9",
-                    "sec-ch-ua": '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
-                    "sec-ch-ua-mobile": "?0",
-                    "sec-fetch-dest": "empty",
-                    "sec-fetch-mode": "cors",
-                    "sec-fetch-site": "same-origin",
-                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
-                },
-                params: { v: id }
+            await this.client.get("https://www.youtube.com/watch", {
+                params: { v: id },
             }).then(async ({ data }) => {
                 const c = this._extract(data).contents.twoColumnWatchNextResults.results.results.contents
                 const v = c.find(item => item.videoPrimaryInfoRenderer).videoPrimaryInfoRenderer
