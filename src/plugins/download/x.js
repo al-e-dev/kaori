d959a517514a93f6108c04c2e033431ddbc7a174
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-26 20:59:16 -0500

    twitter

--- src/plugins/download/x.js
+++ src/plugins/download/x.js
@@ -6,22 +6,23 @@ export default {
     description: 'Descarga y envía imágenes o videos de Twitter',
     comand: ['twitter', 'tw', 'x'],
     exec: async (m, { sock }) => {
-        const { includes: { media } } = await Twitter.download(m.text)
-        const messages = media.map(item => {
-            if (item.type === 'photo') {
-                return { type: 'image', data: { url: item.media_url_https } }
+        await Twitter.download(m.text).then( async ({ includes: { media } }) => {
+            const messages = media.map(item => {
+                if (item.type === 'photo') {
+                    return { type: 'image', data: { url: item.media_url_https } }
+                }
+                if (item.type === 'video') {
+                    const variants = item.variants.filter(v => v.content_type === 'video/mp4')
+                    const best = variants.sort((a, b) => (b.bitrate || 0) - (a.bitrate || 0))[0]
+                    return { type: 'video', data: { url: best ? best.url : item.media_url_https } }
+                }
+                return null
+            }).filter(Boolean)
+            if (messages.length === 1) {
+                await sock.sendMessage(m.from, { [messages[0].type]: { url: messages[0].data.url } })
+            } else if (messages.length > 1) {
+                await sock.sendAlbumMessage(m.from, messages, { delay: 3000 })
             }
-            if (item.type === 'video') {
-                const variants = item.variants.filter(v => v.content_type === 'video/mp4')
-                const best = variants.sort((a, b) => (b.bitrate || 0) - (a.bitrate || 0))[0]
-                return { type: 'video', data: { url: best ? best.url : item.media_url_https } }
-            }
-            return null
-        }).filter(Boolean)
-        if (messages.length === 1) {
-            await sock.sendMessage(m.from, { [messages[0].type]: { url: messages[0].data.url } })
-        } else if (messages.length > 1) {
-            await sock.sendAlbumMessage(m.from, messages, { delay: 3000 })
-        }
+        })
     }
 }
