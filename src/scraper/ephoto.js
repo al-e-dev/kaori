d3eb7bc32dba624c51252dcbf78a4dcb76e67f13
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-27 21:03:00 -0500

    update

--- src/scraper/ephoto.js
+++ src/scraper/ephoto.js
@@ -1,123 +1,82 @@
 import axios from 'axios'
 import * as cheerio from 'cheerio'
 import FormData from 'form-data';
-import cookie from 'cookie';
 
 export default new class Ephoto {
     constructor() {
         this.ephoto = 'https://en.ephoto360.com/create-text-effects-in-the-style-of-the-deadpool-logo-818.html'
     }
 
-    async create(url, texts, radioOption = '') {
-        texts = texts.split(";");
-
-        // Initial GET request to retrieve token and server data
-        const initialResponse = await fetch(url, {
-            method: "GET",
-            headers: {
-                "User-Agent": "GoogleBot",
-            },
-        });
-
-        const initialHtml = await initialResponse.text();
-        const initialCookies = initialResponse.headers
-            .get("set-cookie")
-            .split(",")
-            .map(cookie.parse)
-            .reduce((acc, curr) => ({ ...acc, ...curr }), {});
-        const sessionCookies = Object.entries({
-            __cfduid: initialCookies.__cfduid,
-            PHPSESSID: initialCookies.PHPSESSID,
-        })
-            .map(([key, value]) => cookie.serialize(key, value))
-            .join("; ");
-
-        const $ = cheerio.load(initialHtml);
-        const token = $("input[name=\"token\"]").attr("value");
-        const buildServer = $("input[name=\"build_server\"]").attr("value");
-        const buildServerId = $("input[name=\"build_server_id\"]").attr("value");
-
-        // Récupérer les options pour radio0[radio]
-        const radioOptions = $("input[name^='radio0[radio]']")
-            .map((i, el) => $(el).val())
-            .get();
-
-        // Choisir une option aléatoire si `radioOption` n'est pas fourni
-        if (radioOptions.length > 0 && !radioOption) {
-            const randomIndex = Math.floor(Math.random() * radioOptions.length);
-            radioOption = radioOptions[randomIndex];
-            console.log(`Option radio sélectionnée aléatoirement : ${radioOption}`);
+    async create(url, text) {
+        if (/https?:\/\/(ephoto360|photooxy|textpro)\/\.(com|me)/i.test(url)) throw new Error("URL Invalid")
+        try {
+            let a = await axios.get(url, {
+                headers: {
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                    "Origin": (new URL(url)).origin,
+                    "Referer": url,
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188"
+                }
+            })
+            let $ = cheerio.load(a.data)
+            let server = $('#build_server').val()
+            let serverId = $('#build_server_id').val()
+            let token = $('#token').val()
+            let submit = $('#submit').val()
+            let types = [];
+            $('input[name="radio0[radio]"]').each((i, elem) => {
+                types.push($(elem).attr("value"));
+            })
+            let post;
+            if (types.length != 0) {
+                post = {
+                    'radio0[radio]': types[Math.floor(Math.random() * types.length)],
+                    'submit': submit,
+                    'token': token,
+                    'build_server': server,
+                    'build_server_id': Number(serverId)
+                };
+            }
+            else {
+                post = {
+                    'submit': submit,
+                    'token': token,
+                    'build_server': server,
+                    'build_server_id': Number(serverId)
+                }
+            }
+            let form = new FormData()
+            for (let i in post) {
+                form.append(i, post[i])
+            }
+            if (typeof text == "string") text = [text]
+            for (let i of text) form.append("text[]", i)
+            let b = await axios.post(url, form, {
+                headers: {
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+                    "Origin": (new URL(url)).origin,
+                    "Referer": url,
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188",
+                    "Cookie": a.headers.get("set-cookie").join("; "),
+                    ...form.getHeaders()
+                }
+            })
+            $ = cheerio.load(b.data)
+            let out = ($('#form_value').first().text() || $('#form_value_input').first().text() || $('#form_value').first().val() || $('#form_value_input').first().val())
+            let c = await axios.post((new URL(url)).origin + "/effect/create-image", out, {
+                headers: {
+                    "Accept": "*/*",
+                    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+                    "Origin": (new URL(url)).origin,
+                    "Referer": url,
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188",
+                    "Cookie": a.headers.get("set-cookie").join("; ")
+                }
+            })
+            return { status: c.data?.success, image: server + (c.data?.fullsize_image || c.data?.image || ""), session: c.data?.session_id }
+        } catch (e) {
+            throw e
         }
-
-        // Préparer la requête POST avec les données du formulaire
-        const formData = new FormData();
-        texts.forEach(text => formData.append("text[]", text.trim()));
-        formData.append("submit", "Go");
-        if (radioOption) {
-            formData.append("radio0[radio]", radioOption);
-        }
-        formData.append("token", token);
-        formData.append("build_server", buildServer);
-        formData.append("build_server_id", buildServerId);
-
-        // Envoyer la requête POST
-        const postResponse = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Accept": "*/*",
-                "Accept-Language": "en-US,en;q=0.9",
-                "User-Agent": "GoogleBot",
-                "Cookie": sessionCookies,
-                ...formData.getHeaders(),
-            },
-            body: formData.getBuffer(),
-        });
-
-        const postHtml = await postResponse.text();
-        const postResult = cheerio.load(postHtml);
-
-        const resultValue = url.includes("en.ephoto360.com")
-            ? postResult("input[name=\"form_value_input\"]").attr("value")
-            : postResult("#form_value").first().text();
-
-        if (!resultValue) {
-            return { status: false };
-        }
-
-        const resultData = JSON.parse(resultValue);
-
-        // Préparer la requête finale pour créer l'image
-        const finalFormData = new FormData();
-        finalFormData.append("id", resultData.id);
-        resultData.text.forEach(text => finalFormData.append("text[]", text));
-        finalFormData.append("token", resultData.token);
-        finalFormData.append("build_server", resultData.build_server);
-        finalFormData.append("build_server_id", resultData.build_server_id);
-        if (resultData.radio0) {
-            finalFormData.append("radio0[radio]", resultData.radio0.radio);
-        }
-
-        const finalResponse = await fetch(`${url.split('/').slice(0, 3).join('/')}/effect/create-image`, {
-            method: "POST",
-            headers: {
-                "Accept": "*/*",
-                "Accept-Language": "en-US,en;q=0.9",
-                "User-Agent": "GoogleBot",
-                "Cookie": sessionCookies,
-                ...finalFormData.getHeaders(),
-            },
-            body: finalFormData.getBuffer(),
-        });
-
-        const finalResult = await finalResponse.json();
-
-        if (!finalResult.image) {
-            throw new Error(`textmaker: Erreur lors de la génération de l'image`);
-        }
-        return {
-            status: finalResult.success,
-            url: `${buildServer}${finalResult.image}`,
-        };
-    };
+    }
 
 }
