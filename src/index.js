3eac0025a420f18e3f5f4bc867d3d4078e55dc13
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-28 12:33:04 -0500

    update

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ const start = async () => {
         const date = new Date()
         const Time = `${date.getHours()}:${date.getMinutes()}`
         if (connection == "close") {
-            let reason = new Boom(lastDisconnect?.error)?.output?.statuscode
+            let reason = new Boom(lastDisconnect?.error)?.output?.statusCode
             if (reason == DisconnectReason.badSession) {
                 console.log(chalk.bgRed(`[ ${Time} ] Se daño la carpeta auth, borre la carpeta y escanee el QR nuevamente.`));
                 process.exit()
@@ -159,43 +159,47 @@ const start = async () => {
                         db.data.chats[m.from].cache = db.data.chats[m.from].cache.filter(item => Date.now() - item.timestamp < 1200000)
                     }
 
-                    /* (db.data.chats[m.from]?.antitoxic) {
+                    if (db.data.chats[m.from]?.antitoxic) {
                         let { data: prmpt } = await axios.get("https://raw.githubusercontent.com/al-e-dev/prompt/refs/heads/main/detect.js");
                         
                         let { data } = await axios.post("https://chateverywhere.app/api/chat/", {
-                            "model": {
-                                id: "gpt-4",
-                                name: "GPT-4",
-                                maxLength: 32000,
-                                tokenLimit: 8000,
-                                completionTokenLimit: 5000,
-                                deploymentName: "gpt-4"
-                            },
+                            model: { id: "gpt-4", name: "GPT-4", maxLength: 32000, tokenLimit: 8000, completionTokenLimit: 5000, deploymentName: "gpt-4" },
                             messages: [{ pluginId: null, content: m.body, role: "user" }],
                             prompt: prmpt,
                             temperature: 0.5
                         }, {
-                            headers: {
-                                "Accept": "application/json",
-                                "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
-                            }
+                            headers: { "Accept": "application/json", "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36" }
                         })
 
                         const resultado = typeof data === 'string' ? JSON.parse(data) : data
 
-                        if (resultado.offensive.match) {
+                        if (resultado.obsenity.match) {
+                            if (m.isAdmin) {
+                                m.reply("Stupid admin, no puedes decir eso")
+                                await sock.sendMessage(m.from, { delete: { remoteJid: m.from, fromMe: false, id: m.id, participant: m.sender } })
+                                return
+                            }
+                            m.reply("Se ha detectado un mensaje obsceno y serás eliminado automáticamente.")
+                            await sock.sendMessage(m.from, { delete: { remoteJid: m.from, fromMe: false, id: m.id, participant: m.sender } })
+                            await sock.groupParticipantsUpdate(m.from, [m.sender], "remove")
+                            db.data.users[m.sender].warnings = 0
+                        } else if (resultado.offensive.match) {
+                            if (m.isAdmin) {
+                                m.reply("Stupid admin, no puedes decir eso")
+                                return
+                            }
                             if (db.data.users[m.sender].warnings >= 3) {
-                                m.reply("El mensaje acumula 3 advertencias y será eliminado.")
-                                await sock.groupParticipantsUpdate(m.from, users, "remove")
+                                m.reply("El mensaje acumula 3 advertencias y serás eliminado.")
+                                await sock.sendMessage(m.from, { delete: { remoteJid: m.from, fromMe: false, id: m.id, participant: m.sender } })
+                                await sock.groupParticipantsUpdate(m.from, [m.sender], "remove")
+                                db.data.users[m.sender].warnings = 0
                             } else {
-                                m.reply("Se ha detectado un mensaje ofensivo.")
+                                m.reply(`Se detecto un mensaje ofensivo`)
+                                await sock.sendMessage(m.from, { delete: { remoteJid: m.from, fromMe: false, id: m.id, participant: m.sender } })
                                 db.data.users[m.sender].warnings += 1
                             }
-                        } else if (resultado.obsenity.match) {
-                            m.reply("Se ha detectado un mensaje obsceno y será eliminado automáticamente.");
-                            await sock.groupParticipantsUpdate(m.from, users, "remove")
-                        }
-                    } */
+                        } 
+                    }
                 }
 
                 // console.log(JSON.stringify(m.message, null, 2))
