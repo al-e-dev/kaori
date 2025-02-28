ab3afd7cf14f336dc81217754dc0aa6774d3edfa
Alexito-Hub <alessandrovillogas@outlook.es>
2025-02-27 21:47:48 -0500

    update

--- src/scraper/ephoto.js
+++ src/scraper/ephoto.js
@@ -9,9 +9,10 @@ export default new class Ephoto {
     }
 
     async create(url, text) {
-        if (/https?:\/\/(ephoto360|photooxy|textpro)\/\.(com|me)/i.test(url)) throw new Error("URL Invalid")
-        try {
-            let a = await axios.get(url, {
+        return new Promise(async (resolve, reject) => {
+
+            if (/https?:\/\/(ephoto360|photooxy|textpro)\/\.(com|me)/i.test(url)) throw new Error("URL Invalid")
+            const a = await axios.get(url, {
                 headers: {
                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                     "Origin": (new URL(url)).origin,
@@ -19,64 +20,79 @@ export default new class Ephoto {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188"
                 }
             })
-            let $ = cheerio.load(a.data)
-            let server = $('#build_server').val()
-            let serverId = $('#build_server_id').val()
-            let token = $('#token').val()
-            let submit = $('#submit').val()
-            let types = []
+
+            const $ = cheerio.load(a.data)
+
+            const server = $('#build_server').val()
+            const serverId = $('#build_server_id').val()
+            const token = $('#token').val()
+            const submit = $('#submit').val() ? $('#submit').val() : $('#create_effect').val()
+
+            const types = []
             $('input[name="radio0[radio]"]').each((i, elem) => {
                 types.push($(elem).attr("value"))
             })
-            let post
-            if (types.length != 0) {
+
+            if (types.length !== 0) {
                 post = {
                     'radio0[radio]': types[Math.floor(Math.random() * types.length)],
                     'submit': submit,
                     'token': token,
                     'build_server': server,
                     'build_server_id': Number(serverId)
-                };
-            }
-            else {
+                }
+            } else {
                 post = {
                     'submit': submit,
                     'token': token,
                     'build_server': server,
                     'build_server_id': Number(serverId)
-                }
+                };
             }
-            let form = new FormData()
-            for (let i in post) {
-                form.append(i, post[i])
+            console.log(post)
+
+            const form = new FormData()
+            for (const key in post) {
+                form.append(key, post[key])
             }
-            if (typeof text == "string") text = [text]
-            for (let i of text) form.append("text[]", i)
-            let b = await axios.post(url, form, {
+
+            if (typeof text === "string") text = [text]
+            text.forEach(t => form.append("text[]", t))
+
+            const b = await axios.post(url, form, {
                 headers: {
                     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                     "Origin": (new URL(url)).origin,
                     "Referer": url,
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188",
-                    "Cookie": a.headers.get("set-cookie").join("; "),
+                    "Cookie": a.headers["set-cookie"].join("; "),
                     ...form.getHeaders()
                 }
             })
-            $ = cheerio.load(b.data)
-            let out = ($('#form_value').first().text() || $('#form_value_input').first().text() || $('#form_value').first().val() || $('#form_value_input').first().val())
-            let c = await axios.post((new URL(url)).origin + "/effect/create-image", format(out), {
+
+            const c$ = cheerio.load(b.data);
+            const out = c$('#form_value').first().text() || c$('#form_value_input').first().text() || c$('#form_value').first().val() || c$('#form_value_input').first().val();
+
+            console.log(JSON.parse(out))
+            const c = await axios.post((new URL(url)).origin + "/effect/create-image", JSON.parse(out), {
                 headers: {
                     "Accept": "*/*",
                     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
                     "Origin": (new URL(url)).origin,
                     "Referer": url,
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188",
-                    "Cookie": a.headers.get("set-cookie").join("; ")
+                    "Cookie": a.headers["set-cookie"].join("; ")
                 }
             })
-            return { status: c.data?.success, image: server + (c.data?.fullsize_image || c.data?.image || ""), session: c.data?.session_id }
-        } catch (e) {
-            throw e
-        }
+
+            console.log(c.data)
+
+            resolve({
+                status: c.data?.success,
+                info: c.data?.info,
+                url: server + (c.data?.fullsize_image || c.data?.image || ""),
+                session: c.data?.session_id
+            })
+        })
     }
 }
