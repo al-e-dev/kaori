3a8066d4f390deab73381d13000a454afb77e487
Alessandro <95830927+al-e-dev@users.noreply.github.com>
2025-03-02 01:08:17 -0500

    Actualizar convert

---
+++ src/plugins/convert/brat.js
@@ -0,0 +1,13 @@
+export default {
+    name: 'brat',
+    description: 'Convierte texto en brat',
+    comand: ['brat'],
+    os: true,
+    exec: async (m, { sock, Scrap }) => {
+        await sock.sendSticker(m.from, {
+            image: await Scrap._convert.brat(m.text),
+            packname: m.pushName || 'annonimous',
+            author: sock.user.name
+        }, { quoted: m })
+    }
+}
